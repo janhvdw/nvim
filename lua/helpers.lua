47d882cac685f10099cf90c0f3a250178352c762
Jan van_der_Westhuizen <Jan.van_der_Westhuizen@partner.bmwgroup.co.za>
2023-05-03 17:14:07 +0200

    add is windows utility

--- lua/helpers.lua
+++ lua/helpers.lua
@@ -1,5 +1,5 @@
 local is_wsl = (function()
-  local output = vim.fn.systemlist "uname -r"
+  local output = vim.fn.systemlist("uname -r")
   return not not string.find(output[1] or "", "WSL")
 end)()
 
@@ -7,7 +7,11 @@ local is_mac = vim.fn.has("macunix") == 1
 
 local is_linux = not is_wsl and not is_mac
 
+local is_windows = vim.fn.has("win32")
 
 return {
-  is_wsl = is_wsl, is_mac = is_mac, is_linux = is_linux
+  is_wsl = is_wsl,
+  is_mac = is_mac,
+  is_linux = is_linux,
+  is_windows = is_windows,
 }
