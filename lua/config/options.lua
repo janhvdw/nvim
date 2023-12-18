984e8909e56be48763d3034ee4602ee2a24a0b15
Jan van_der_Westhuizen <Jan.van_der_Westhuizen@partner.bmwgroup.co.za>
2023-12-18 10:38:54 +0100

    add neovide options

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -20,3 +20,21 @@ if helpers.iswsl then
     cache_enabled = false,
   }
 end
+
+if vim.g.neovide == true then
+  vim.g.neovide_scale_factor = 0.7
+  vim.o.guifont = "JetBrains Mono"
+  vim.api.nvim_set_keymap(
+    "n",
+    "<C-+>",
+    ":lua vim.g.neovide_scale_factor = vim.g.neovide_scale_factor + 0.1<CR>",
+    { silent = true }
+  )
+  vim.api.nvim_set_keymap(
+    "n",
+    "<C-->",
+    ":lua vim.g.neovide_scale_factor = vim.g.neovide_scale_factor - 0.1<CR>",
+    { silent = true }
+  )
+  vim.api.nvim_set_keymap("n", "<C-0>", ":lua vim.g.neovide_scale_factor = 1<CR>", { silent = true })
+end
