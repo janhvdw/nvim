c123216bc4a553c2783c35fb32f2c761ad19311d
Jan van_der_Westhuizen <jan.vander.westhuizen@capgemini.com>
2024-04-25 08:47:57 +0200

    Add copilot and update plugins

---
+++ lua/plugins/ai.lua
@@ -0,0 +1,36 @@
+return {
+  {
+    "CopilotC-Nvim/CopilotChat.nvim",
+    branch = "canary",
+    dependencies = {
+      { "zbirenbaum/copilot.lua" }, -- or github/copilot.vim
+      { "nvim-lua/plenary.nvim" }, -- for curl, log wrapper
+    },
+    keys = {
+      {
+        "<leader>cpq",
+        function()
+          local input = vim.fn.input("Quick Chat: ")
+          if input ~= "" then
+            require("CopilotChat").ask(input, { selection = require("CopilotChat.select").buffer })
+          end
+        end,
+        desc = "CopilotChat - Quick chat",
+      },
+    },
+    opts = {
+      debug = true, -- Enable debugging
+      -- See Configuration section for rest
+    },
+    -- See Commands section for default commands if you want to lazy load on them
+  },
+  {
+    "zbirenbaum/copilot.lua",
+    cmd = "Copilot",
+    build = ":Copilot auth",
+    config = function(_, opts)
+      require("copilot").setup(opts)
+      vim.cmd("Copilot disable")
+    end,
+  },
+}
