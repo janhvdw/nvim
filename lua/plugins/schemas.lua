2ecc4de8fad62ec2c90608790d99b7d049eb6dc5
Jan van_der_Westhuizen <jan.vander.westhuizen@capgemini.com>
2024-06-17 07:44:32 +0200

    replace lang.yaml lazy extra

---
+++ lua/plugins/schemas.lua
@@ -0,0 +1,69 @@
+return {
+  -- yaml schema support
+  {
+    "b0o/SchemaStore.nvim",
+    lazy = true,
+    version = false, -- last release is way too old
+  },
+
+  -- correctly setup lspconfig
+  {
+    "neovim/nvim-lspconfig",
+    dependencies = { "b0o/SchemaStore.nvim" },
+    opts = {
+      -- make sure mason installs the server
+      servers = {
+        yamlls = {
+          -- Have to add this for yamlls to understand that we support line folding
+          capabilities = {
+            textDocument = {
+              foldingRange = {
+                dynamicRegistration = false,
+                lineFoldingOnly = true,
+              },
+            },
+          },
+          -- lazy-load schemastore when needed
+          on_new_config = function(new_config)
+            new_config.settings.yaml.schemas = vim.tbl_deep_extend(
+              "force",
+              new_config.settings.yaml.schemas or {},
+              require("schemastore").yaml.schemas({
+                replace = {
+                  ["Azure Pipelines"] = {
+                    name = "Azure Pipelines",
+                    description = "Azure Pipelines YAML pipelines definition",
+                    fileMatch = {
+                      "azure-pipelines.yml",
+                      "azure-pipelines.yaml",
+                      "**/.azure/**/*.yml",
+                      "**/.azure/**/*.yaml",
+                    },
+                    url = "https://raw.githubusercontent.com/microsoft/azure-pipelines-vscode/master/service-schema.json",
+                  },
+                },
+              })
+            )
+          end,
+          settings = {
+            redhat = { telemetry = { enabled = false } },
+            yaml = {
+              keyOrdering = false,
+              format = {
+                enable = true,
+              },
+              validate = true,
+              schemaStore = {
+                -- Must disable built-in schemaStore support to use
+                -- schemas from SchemaStore.nvim plugin
+                enable = false,
+                -- Avoid TypeError: Cannot read properties of undefined (reading 'length')
+                url = "",
+              },
+            },
+          },
+        },
+      },
+    },
+  },
+}
