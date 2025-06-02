90cbdb94c621d13e5ea09d7992ae848d20238eb6
Jan van_der_Westhuizen <jan.vander.westhuizen@capgemini.com>
2025-06-02 09:32:56 +0200

    lazy upgrade

--- lua/plugins/java.lua
+++ lua/plugins/java.lua
@@ -1,13 +1,13 @@
 -- requires on java extra from lazy vim
 return {
   -- { "nvim-java/nvim-java" },
-  {
-    "mfussenegger/nvim-jdtls",
-    opts = {
-      cmd = {
-        "jdtls",
-        "--jvm-arg=" .. string.format("-javaagent:%s", vim.fn.stdpath("data") .. "/mason/share/jdtls/lombok.jar"),
-      },
-    },
-  },
+  -- {
+  --   "mfussenegger/nvim-jdtls",
+  --   opts = {
+  --     cmd = {
+  --       "jdtls",
+  --       "--jvm-arg=" .. string.format("-javaagent:%s", vim.fn.stdpath("data") .. "/mason/share/jdtls/lombok.jar"),
+  --     },
+  --   },
+  -- },
 }
