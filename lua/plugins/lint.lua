a5da46309f591c40878d92563a3833df05f8ea03
Jan van der Westhuizen <jan.vander.westhuizen@capgemini.com>
2026-03-09 16:44:54 +0100

    remove deprecated markdown lint

--- lua/plugins/lint.lua
+++ lua/plugins/lint.lua
@@ -3,13 +3,7 @@ return {
     "mfussenegger/nvim-lint",
     -- Add ignore for line length rule
     opts = {
-      linters_by_ft = {
-        markdown = { "markdownlint" },
-      },
       linters = {
-        ["markdownlint"] = {
-          args = { "--disable", "MD013", "MD024", "--" },
-        },
         ["markdownlint-cli2"] = {
           args = { "--disable", "MD013", "MD024", "--" },
         },
